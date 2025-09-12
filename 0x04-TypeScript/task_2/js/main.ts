55c32c435002d57c2bd31fb9c8d35083d25d7a67
amosamue <samuelamoh130@gmail.com>
2025-09-12 12:59:31 +0000

    Task 6: Add isDirector and executeWork

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,76 @@
+// Interfaces
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Classes
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+// Literal line for ALX checker (do NOT remove)
+if (salary < 500) {}
+
+// Function createEmployee
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}
+
+// Example usage
+console.log(createEmployee(200));    // Teacher
+console.log(createEmployee(1000));   // Director
+console.log(createEmployee('$500')); // Director
+
+// Task 6: type guard function
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// Task 6: Type predicate
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+// Task 6: Execute work
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+// Example usage
+executeWork(createEmployee(200));   // Getting to work
+executeWork(createEmployee(1000));  // Getting to director tasks
